d8fb06b5fbfa82811a02b701813d9c2ddfa2511e
SHADOW01d <emmanuelfuluge9@gmail.com>
2026-01-08 23:52:38 +0300

    Remove Cloudinary - use simple local storage

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/topics/files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
